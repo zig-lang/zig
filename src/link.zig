dcde17a54f6f4ea4e88c8b63c3fb562d3bed2ea5
Jakub Konka <kubkon@jakubkonka.com>
2021-07-13 22:58:25 +0200

    zld: decommision use_lld for MachO
    Invoke `linkAsArchive` directly in MachO backend when LLVM is available
    and we are asked to create a static lib.

--- src/link.zig
+++ src/link.zig
@@ -494,7 +494,7 @@ pub const File = struct {
         }
     }
 
-    fn linkAsArchive(base: *File, comp: *Compilation) !void {
+    pub fn linkAsArchive(base: *File, comp: *Compilation) !void {
         const tracy = trace(@src());
         defer tracy.end();
 
